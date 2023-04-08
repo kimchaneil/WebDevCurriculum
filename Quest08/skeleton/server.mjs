b57c903b38fc52c8a589fa2f15ed0cbc83badb0a
kimchaneil <chaneil2@naver.com>
2023-04-09 00:14:12 +0900

    quest08_GET

--- Quest08/skeleton/server.mjs
+++ Quest08/skeleton/server.mjs
@@ -1,12 +1,15 @@
 import http from 'http';
 import url from 'url';
+
 const server = http.createServer((req, res) => {
-  const {pathname,query}= url.parse(req.url,true);
-  if(req.method == 'GET' && pathname == '/foo' && query.bar){
-    res.write
+  const { pathname, query } = url.parse(req.url, true);
+  if (req.method === 'GET' && pathname === '/foo' && query.bar) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end(`Hello,` + `${query.bar}`);
+  } else {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World!');
   }
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!');
 });
 
 server.listen(8080);
